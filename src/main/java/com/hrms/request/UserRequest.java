c296c9d327c3939ca91e9e427363980b0e988f18
rajeshskywave <rajeshr@skywavessoftware.com>
2024-05-16 21:32:13 +0530

    User Request added fields

--- src/main/java/com/hrms/request/UserRequest.java
+++ src/main/java/com/hrms/request/UserRequest.java
@@ -1,5 +1,20 @@
 package com.hrms.request;
 
+import javax.persistence.Entity;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class UserRequest {
+	
+	private String username;
+	private String password;
 
 }
