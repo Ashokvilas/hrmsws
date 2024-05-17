1d242ac9a556157478b479cbffee04f8c53869e5
rajeshskywave <rajeshr@skywavessoftware.com>
2024-05-17 12:14:38 +0530

    Updated fields in User Request and Service Status Response

--- src/main/java/com/hrms/request/UserRequest.java
+++ src/main/java/com/hrms/request/UserRequest.java
@@ -17,6 +17,7 @@ public class UserRequest {
 	private String username;
 	private String password;
 	private String phonenumber;
+	private String email;
 	
 	
 
--- src/main/java/com/hrms/response/ServiceStatus.java
+++ src/main/java/com/hrms/response/ServiceStatus.java
@@ -2,4 +2,8 @@ package com.hrms.response;
 
 public class ServiceStatus {
 
+	private String statusCode;
+	private String statusDescription;
+	
+	
 }
